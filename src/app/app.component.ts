bb9fead3f6f242aad2afdf782bddcf55ead90119
juan manuel <jmgv87191@gmail.com>
2024-02-23 11:28:57 -0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CurriculumComponent } from './components/curriculum/curriculum.component';
+import { TrabajosComponent } from './components/trabajos/trabajos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,CurriculumComponent,TrabajosComponent, ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { CurriculumComponent } from './components/curriculum/curriculum.component';
+import { TrabajosComponent } from './components/trabajos/trabajos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'contacto',
+        component: ContactoComponent
+    },
+    {
+        path: 'curriculum',
+        component: CurriculumComponent
+    },
+    {
+        path: 'trabajos',
+        component: TrabajosComponent
+    },
+];
